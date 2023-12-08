43d503224e5e0734b8c0fd0b4f8c6a913419a114
TBXark <tbxark@outlook.com>
2023-12-08 11:34:08 +0800

    perf: 修改新文章判断逻辑

--- src/index.js
+++ src/index.js
@@ -23,25 +23,33 @@ export default {
 	async scheduled(event, env, ctx) {
 		const { DB, TELEGRAM_ID, TELEGRAM_TOKEN } = env
 		const result = await loadData();
-		const lastArticle = await DB.get('lastArticle')
-		const newArticle = result[0]
-		if (lastArticle === newArticle.link) {
-			return
+		const lastArticles = await DB.get('lastArticles').then(res => res || '[]').then(JSON.parse)
+		for (const newArticle of result) {
+			try {
+				if (lastArticles.includes(newArticle.link)) {
+					continue
+				}
+				lastArticles.push(newArticle.link)
+				const message = `${newArticle.summary}\n\n${newArticle.link}`
+				await fetch(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+					method: 'POST',
+					headers: {
+						'content-type': 'application/json;charset=UTF-8',
+					},
+					body: JSON.stringify({
+						chat_id: TELEGRAM_ID,
+						text: message,
+						disable_web_page_preview: false,
+					}),
+				})
+			} catch (e) {
+				console.log(e)
+			}
 		}
-		await DB.put('lastArticle', newArticle.link)
-		const message = `${newArticle.summary}\n\n${newArticle.link}`
-		await fetch(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
-			method: 'POST',
-			headers: {
-				'content-type': 'application/json;charset=UTF-8',
-			},
-			body: JSON.stringify({
-				chat_id: TELEGRAM_ID,
-				text: message,
-				disable_web_page_preview: false,
-			}),
-		})
-		
+		if (lastArticles.length > 50) {
+			lastArticles.splice(0, lastArticles.length - 50)
+		}
+		await DB.put('lastArticles', lastArticles)
 	},
 	async fetch(request, env, ctx) {
 		const result = await loadData();
